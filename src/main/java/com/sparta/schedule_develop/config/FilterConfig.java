706b7f56b28d76cee330943711fd04538a5ba6be
Uguls <chobozaex@gmail.com>
2025-03-27 19:03:14 +0900

    feat: Lv4 로그인 필터 구현

---
+++ src/main/java/com/sparta/schedule_develop/config/FilterConfig.java
@@ -0,0 +1,19 @@
+package com.sparta.schedule_develop.config;
+
+import com.sparta.schedule_develop.filter.LoginFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterConfig {
+
+    @Bean
+    public FilterRegistrationBean<LoginFilter> loginFilter() {
+        FilterRegistrationBean<LoginFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new LoginFilter());
+        registrationBean.addUrlPatterns("/*");
+        registrationBean.setOrder(1);
+        return registrationBean;
+    }
+}
