6da88faf096429bddf90ada1bd7247169f8f8ac2
Uguls <chobozaex@gmail.com>
2025-03-27 13:50:44 +0900

    feat: schedule 엔티티 생성

---
+++ src/main/java/com/sparta/schedule_develop/entity/Schedule.java
@@ -0,0 +1,22 @@
+package com.sparta.schedule_develop.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "schedule")
+public class Schedule extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+}
